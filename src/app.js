4e60c4dc265d3b33b7b9bdb53579003388ad3327
Robert Tam <rsh.tam@gmail.com>
2019-06-24 23:33:05 +0100

    Added more css and implemented checking off completed items

--- src/app.js
+++ src/app.js
@@ -1,25 +1,41 @@
 import Vue from "vue";
+import TextFormat from "./helpers/text_format.js"
 
 document.addEventListener("DOMContentLoaded", () => {
   new Vue({
     el: "#app",
     data: {
       todos: [
-        {name: "Laundry", priority: "high"},
-        {name: "Shopping", priority: "high"},
-        {name: "Wash Car", priority: "low"}
+        {name: "Laundry", priority: "high", completed: true},
+        {name: "Shopping", priority: "high", completed: false},
+        {name: "Wash Car", priority: "low", completed: false},
+        {name: "Take out rubbish", priority: "low", completed: false},
+        {name: "Buy airplane tickets", priority: "low", completed: false},
+        {name: "Book hotel", priority: "low", completed: false},
+        {name: "Book restaurant reservation", priority: "low", completed: false},
+        {name: "Appointment with bank", priority: "low", completed: false},
+        {name: "Dentist appointment", priority: "low", completed: false},
+        {name: "Find new flat", priority: "low", completed: false},
+        {name: "Buy lotto ticket", priority: "low", completed: false}
       ],
       newItem: "",
-      newPriority: ""
+      newPriority: "",
     },
     methods: {
       addToList: function () {
-        this.todos.push({name: this.newItem, priority: this.newPriority});
+        this.todos.push({
+          name: TextFormat.capitalise(this.newItem),
+          priority: this.newPriority,
+          completed: false
+        });
         this.newItem = "";
         this.newPriority = "";
       },
       setPriority: function (index) {
         this.todos[index].priority = this.todos[index].priority === "low" ? "high" : "low";
+      },
+      setCompleted: function (index) {
+        this.todos[index].completed = this.todos[index].completed === true ? false : true;
       }
     }
   });
